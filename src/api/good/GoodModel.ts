4ee6faaface1d0741c4d83cd60a1b633dd452f5e
张雨润 <13855587+yurun-zhang@user.noreply.gitee.com>
2024-08-16 23:22:07 +0800

    完成购买功能

--- src/api/good/GoodModel.ts
+++ src/api/good/GoodModel.ts
@@ -1,28 +1,31 @@
 export type GoodModel = {
-  goodId:string,
-  goodName:string,
-  description:string,
-  price:string,
-  pictureKey:string,
-  categoryId:string,
-  createTime:string,
-  updateTime:string,
+    goodId: string,
+    goodName: string,
+    description: string,
+    price: string,
+    pictureKey: string,
+    categoryId: string,
+    createTime: string,
+    updateTime: string,
 
 };
-export type GoodListParam={
-    goodName:string,
-    description:string,
-    price:number,
-    currentPage:number,
-    pageSize:number,
-    total:number
+export type GoodListParam = {
+    goodName: string,
+    description: string,
+    price: number,
+    currentPage: number,
+    pageSize: number,
+    total: number
 }
-export type GoodCondition={
-  categoryIds:string,
-  priceMin:string,
-  priceMax:string
+export type GoodCondition = {
+    categoryIds: string,
+    priceMin: string,
+    priceMax: string
 }
-export type shopModel={
-  userId:string,
-  goodId:string,
+export type shopModel = {
+    userId: string,
+    goodId: string,
+    buyNum: number,
+    sum: number,
+    address: string,
 }
--- src/views/goods/GoodsList.vue
+++ src/views/goods/GoodsList.vue
@@ -17,10 +17,10 @@
     </el-form>
     <!-- 新增编辑弹窗 -->
     <SysDialog :title="dialog.title" :visible="dialog.visible" :width="dialog.width" :height="dialog.height"
-      @on-close="onClose" @on-confirm="commit">
+               @on-close="onClose" @on-confirm="commit">
       <template v-slot:content>
-        <el-form :model="addModel" ref="addRef" :rules="rules" label-width="100px" :inline="false" size="default"
-          style="height:290px;overflow:auto;">
+        <el-form :model="addModel" ref="addRef" :rules="rules" label-width="100px" size="default"
+                 style="height:290px;overflow:auto;">
           <el-form-item label="商品图片">
             <el-upload action="http://localhost:8080/api/files/upload" :on-success="uploadImg">
               <el-tooltip content="PNG,JPEG 文件小于 500KB" placement="right">
@@ -34,14 +34,18 @@
           <el-form-item label="商品类别">
             <!--复选框-->
             <SelectChecked :bindValue="bindValue" ref="selectRef" :options="options" @selected="selected"
-              style="width:200px">
+                           style="width:200px">
             </SelectChecked>
           </el-form-item>
           <el-form-item label="商品描述" prop="description">
-            <el-input v-model="addModel.description" width="200px" type="textarea" placeholder="请输入商品相关描述" />
+            <el-input v-model="addModel.description" type="textarea" placeholder="请输入商品相关描述"
+                      style="width: 400px"/>
           </el-form-item>
           <el-form-item label="商品价格" prop="price" style="width:300px">
             <el-input v-model="addModel.price" placeholder="单位:元"></el-input>
+          </el-form-item>
+          <el-form-item label="商品库存" prop="storeNum" style="width:300px">
+            <el-input v-model="addModel.storeNum" placeholder="单位:个"></el-input>
           </el-form-item>
         </el-form>
       </template>
@@ -51,36 +55,49 @@
       <el-table-column label="商品图片" width="150">
         <template #default="scope">
           <el-image style="width: 50px; height: 70px" :src="'http://localhost:8080/api/files/' + scope.row.pictureKey"
-            :preview-src-list="['http://localhost:8080/api/files/' + scope.row.pictureKey]" :preview-teleported="true">
+                    :preview-src-list="['http://localhost:8080/api/files/' + scope.row.pictureKey]"
+                    :preview-teleported="true">
           </el-image>
         </template>
       </el-table-column>
-      <el-table-column prop="goodName" label="商品名称" width="200"></el-table-column>
-      <el-table-column prop="description" label="商品描述" width="400"></el-table-column>
+      <el-table-column prop="goodName" label="商品名称" width="150"></el-table-column>
+      <el-table-column prop="description" label="商品描述" width="250"></el-table-column>
       <el-table-column prop="price" label="价格/(元)" width="100"></el-table-column>
-      <el-table-column v-if="global.$hasPerm(['sys:good:edit', 'sys:good:delete'])" label="操作" align="center" >
+      <el-table-column prop="storeNum" label="库存/(个)" width="100">
         <template #default="scope">
-          <el-button icon="Select" type="default" size="default" @click="buyBtn(scope.row.goodId)">购买</el-button>
+          <div style="display: flex; justify-content: center;">
+            <el-tag type="success" effect="plain" style="margin: 0 10px;">{{ scope.row.storeNum }}</el-tag>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column v-if="global.$hasPerm(['sys:good:edit', 'sys:good:delete'])" label="操作" align="center">
+        <template #default="scope">
+          <el-button icon="Select" type="default" size="default"
+                     @click="buyBtn(scope.row.goodId,scope.row.goodName,scope.row.price,scope.row.storeNum)">购买
+          </el-button>
           <el-button v-if="global.$hasPerm(['sys:good:edit'])" type="primary" size="default" icon="Edit"
-            @click="editBtn(scope.row)">编辑</el-button>
+                     @click="editBtn(scope.row)">编辑
+          </el-button>
           <el-button v-if="global.$hasPerm(['sys:good:delete'])" type="danger" size="default" icon="Delete"
-            @click="deleteBtn(scope.row.goodId)">删除</el-button>
+                     @click="deleteBtn(scope.row.goodId)">删除
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
     <!-- 分页-->
     <el-pagination @size-change="sizeChange" @current-change="currentChange" :current-page="searchParam.currentPage"
-      :page-sizes="[10, 20, 30, 40]" :page-size="searchParam.pageSize" layout="total, sizes, prev, pager, next, jumper"
-      :total="searchParam.total">
+                   :page-sizes="[10, 20, 30, 40]" :page-size="searchParam.pageSize"
+                   layout="total, sizes, prev, pager, next, jumper"
+                   :total="searchParam.total">
     </el-pagination>
     <!-- 筛选弹窗 -->
     <SysDialog :title="filterTitle" :visible="filterVisible" :width="500" :height="150" @on-close="filterClose"
-      @on-show="filterShow" @on-confirm="filterConfirm">
+               @on-show="filterShow" @on-confirm="filterConfirm">
       <template v-slot:content>
         <el-form :model="filterModel" label-width="80px" :inline="false" size="default">
           <el-form-item label="商品类别">
             <el-select v-model="filterValueOptions" multiple placeholder="请选择商品类别" style="width:300px"
-              @change="filterChange">
+                       @change="filterChange">
               <el-option v-for="item in filterOptions" :key="item.value" :label="item.label" :value="item.value">
               </el-option>
             </el-select>
@@ -93,32 +110,58 @@
         </el-form>
       </template>
     </SysDialog>
+    <!--商品购买弹框-->
+    <SysDialog :title="buyModel.title" :visible="buyModel.visible" :width="500" :height="300" @on-close="onCloseBuy"
+               @on-confirm="onConfirmBuy">
+      <template v-slot:content>
+        <el-form :model="buyModel" ref="bugRef" label-width="100px" size="default"
+                 style="height:290px;overflow:auto;">
+          <el-form-item label="商品名称" prop="goodName" style="width:300px">
+            <el-input disabled v-model="buyModel.goodName"></el-input>
+          </el-form-item>
+          <el-form-item label="商品价格" prop="price" style="width:300px">
+            <el-input disabled v-model="buyModel.sum" placeholder="单位:元"></el-input>
+          </el-form-item>
+          <el-form-item label="购买数量" prop="storeNum" style="width:230px;">
+            <div style="width: 100%;height:100%;display: flex;justify-content: space-between ">
+              <el-button @click="buyModel.buyNum>0?buyModel.buyNum--:0">-</el-button>
+              <el-input v-model="buyModel.buyNum" placeholder="单位:个" style="width: 40px"></el-input>
+              <el-button @click="buyModel.buyNum<buyModel.storeNum?buyModel.buyNum++:buyModel.storeNum">+</el-button>
+            </div>
+          </el-form-item>
+          <el-form-item label="配送地址" prop="address" style="width:400px">
+            <el-input v-model="buyModel.address" placeholder="请填写详细家庭住址"></el-input>
+          </el-form-item>
+        </el-form>
+      </template>
+    </SysDialog>
   </el-main>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, onMounted, nextTick, computed, watch, watchEffect } from 'vue';
+import {ref, reactive, onMounted, nextTick, computed, watch, watchEffect} from 'vue';
 import SysDialog from '@/components/SysDialog.vue'
 import useDialog from '@/hooks/useDialog'
-import { getListApi, addGoodApi, deleteGoodApi, editGoodApi, getCategoryListApi, getGoodConditionApi } from '@/api/good'
+import {getListApi, addGoodApi, deleteGoodApi, editGoodApi, getCategoryListApi, getGoodConditionApi} from '@/api/good'
 import {shopCartApi} from '@/api/user'
-import { getSelectApi } from '@/api/category'
-import { type FormInstance, ElMessage } from 'element-plus';
-import type { GoodModel } from '@/api/good/GoodModel';
+import {getSelectApi} from '@/api/category'
+import {type FormInstance, ElMessage} from 'element-plus';
+import type {GoodModel} from '@/api/good/GoodModel';
 import useInstance from '@/hooks/useInstance';
 import SelectChecked from "@/components/SelectChecked.vue";
-import { getRoleListApi } from "@/api/user";
-import type { List } from 'echarts';
-import { type shopModel } from '@/api/good/GoodModel';
-import { useUserStore } from '@/stores/user';
+import {getRoleListApi} from "@/api/user";
+import type {List} from 'echarts';
+import {type shopModel} from '@/api/good/GoodModel';
+import {useUserStore} from '@/stores/user';
 
 const userStore = useUserStore()
-const { global } = useInstance()
-const { dialog, onClose, onConfirm, onShow } = useDialog()
+const {global} = useInstance()
+const {dialog, onClose, onConfirm, onShow} = useDialog()
 const searchParam = reactive({
   goodName: '',
   description: '',
   price: 0,
+  storeNum: 0,
   currentPage: 1,
   pageSize: 10,
   total: 0
@@ -129,12 +172,28 @@ const addModel = reactive({
   goodName: '',
   description: '',
   price: '',
+  storeNum: '',
   pictureKey: '',
   createTime: '',
   updateTime: '',
   categoryId: ''
 })
 
+const buyRef = ref<FormInstance>()
+const buyModel = reactive({
+  title: '商品购买',
+  visible: false,
+  userId: userStore.userId,
+  goodId: '',
+  goodName: '',
+  storeNum: 0,
+  buyNum: 1,
+  price: 0,
+  sum: computed(() => {
+    return buyModel.price * buyModel.buyNum
+  }),
+  address: ''
+})
 // 表单ref
 const addRef = ref<FormInstance>()
 const rules = reactive({
@@ -147,8 +206,14 @@ const rules = reactive({
     required: true,
     message: '请输入价格',
     trigger: ['blur', 'change']
+  }],
+  storeNum: [{
+    required: true,
+    message: '请输入库存',
+    trigger: ['blur', 'change']
   }]
 })
+
 
 //搜索
 const searchBtn = () => {
@@ -232,6 +297,7 @@ const commit = () => {
 }
 //table列表查询
 const tableList = ref([])
+
 async function getList() {
   let res = await getListApi(searchParam)
   if (res && res.code == 200) {
@@ -245,12 +311,14 @@ const bindValue = ref([])
 const selectRef = ref()
 //下拉菜单类
 const options = ref([])
+
 async function getSelect() {
   let res = await getSelectApi()
   if (res && res.code == 200) {
     options.value = res.data;
   }
 }
+
 //存取已经对应的分类id，以“，”分隔
 const categoryIds = ref('')
 //根据商品id查询对应分类
@@ -293,7 +361,6 @@ const filterConfirm = async () => {
   let res = await getGoodConditionApi(filterModel)
   if (res && res.code == 200) {
     tableList.value = res.data
-    console.log(res.data);
 
   }
   filterClose()
@@ -309,6 +376,7 @@ const filterOptions = ref([{
   label: '',
   check: ''
 }])
+
 //获取所有分类选项
 function getFilterOptions() {
   getSelect().then(() => {
@@ -318,7 +386,6 @@ function getFilterOptions() {
 
 const filterChange = () => {
   filterModel.categoryIds = filterValueOptions.value.join(',')
-  console.log(filterModel.categoryIds);
 
 }
 
@@ -328,17 +395,34 @@ function uploadImg(res: any) {
 
 }
 
-
-async function buyBtn(goodId:string){
-  console.log(userStore.getUserId);
-  console.log(goodId);
-  let res = await shopCartApi({userId:userStore.getUserId,goodId:goodId})
-  if(res&&res.code==200){
-    ElMessage.success(res.msg)
-  }
+// 显示购买弹框，并传递参数
+ function buyBtn(goodId: string, goodName: string, price: number, storeNum: number) {
+  buyModel.visible = true;
+  buyModel.goodId = goodId;
+  buyModel.goodName = goodName;
+  buyModel.price = price;
+  buyModel.storeNum = storeNum
 }
 
-
+const onCloseBuy = () => {
+  buyModel.buyNum = 0;
+  buyModel.address = ''
+  buyModel.visible = false
+}
+const onConfirmBuy = async () => {
+  let buyParam = {
+    userId: userStore.getUserId,
+    goodId: buyModel.goodId,
+    buyNum: buyModel.buyNum,
+    sum: buyModel.sum,
+    address: buyModel.address
+  }
+  let res = await shopCartApi(buyParam)
+  if (res && res.code == 200) {
+    ElMessage.success(res.msg)
+  }
+  onCloseBuy()
+}
 
 //页容量改变时触发
 const sizeChange = (size: number) => {
